c78b065364e01e650407f80829bf22e746265fc2
Yaamini Venkataraman <yaaminiv@uw.edu>
2020-03-03 08:45:09 -0800

    reran GO-MWU without signed pvalues

--- analyses/GO-MWU/Zostera-GO_MWU.R
+++ analyses/GO-MWU/Zostera-GO_MWU.R
@@ -20,7 +20,7 @@
 
 #### GO-BP ####
 
-input="2019-07-11-Zostera-Table-of-Significance-Measures.csv" # two columns of comma-separated values: gene id, continuous measure of significance. To perform standard GO enrichment analysis based on Fisher's exact test, use binary measure (0 or 1, i.e., either sgnificant or not).
+input="2019-07-11-Zostera-Table-of-Significance-Measures-Uncorrected.csv" # two columns of comma-separated values: gene id, continuous measure of significance. To perform standard GO enrichment analysis based on Fisher's exact test, use binary measure (0 or 1, i.e., either sgnificant or not).
 goAnnotations="2019-07-11-Zostera-GO-Annotations-Table.tab" # two-column, tab-delimited, one line per gene, multiple GO terms separated by semicolon. If you have multiple lines per gene, use nrify_GOtable.pl prior to running this script.
 goDatabase="go.obo" # download from http://www.geneontology.org/GO.downloads.ontology.shtml
 goDivision="BP" # either MF, or BP, or CC
